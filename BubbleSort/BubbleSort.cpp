13891dba525007706ee1fe921089f6107bf15396
racinewu <racinewu@gmail.com>
2025-05-31 04:44:15 +0800

    first commit

---
+++ BubbleSort/BubbleSort.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+using namespace std;
+using namespace chrono;
+
+void bubbleSort(vector<int> &arr)
+{
+    int n = arr.size();
+    bool swapped;
+    for (int i = 0; i < n - 1; ++i)
+    {
+        swapped = false;
+        for (int j = 0; j < n - i - 1; ++j)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        cerr << "Usage: " << argv[0] << " input.txt output.txt\n";
+        return 1;
+    }
+
+    string inputFile = argv[1];
+    string outputFile = argv[2];
+
+    vector<int> arr;
+    int x;
+
+    ifstream infile(inputFile);
+    if (!infile)
+    {
+        cerr << "Failed to open " << inputFile << '\n';
+        return 1;
+    }
+
+    while (infile >> x)
+        arr.push_back(x);
+    infile.close();
+
+    auto start = high_resolution_clock::now();
+    bubbleSort(arr);
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(end - start);
+    cout << "Execution: " << duration.count() << " ms" << endl;
+
+    ofstream outfile(outputFile);
+    if (!outfile)
+    {
+        cerr << "Failed to open " << outputFile << '\n';
+        return 1;
+    }
+
+    for (int val : arr)
+        outfile << val << '\n';
+    outfile.close();
+
+    return 0;
+}
